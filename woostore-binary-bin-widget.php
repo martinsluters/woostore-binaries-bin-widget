7945a854af23e23c085c110e4ae7d480826d5034
Martins Luters <luters.martins@gmail.com>
2023-04-30 11:42:24 +0300

    Add plugin's main PHP file

---
+++ woostore-binary-bin-widget.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: WooStore Binary Bin Widget
+ * Description: Binary Bin Widget for WooCommerce powered stores.
+ * Plugin URI: https://github.com/martinsluters/woostore-binary-bin-widget
+ * Author: Martins Luters
+ * Author URI: https://github.com/martinsluters
+ * Version: 1.0.0
+ * License: MIT
+ * Text Domain: woostore-binary-bin-widget
+ *
+ * @package     WooStoreBinaryBinWidget
+ * @author      Martins Luters (luters.martins@gmail.com)
+ * @license     MIT
+ *
+ * @wordpress-plugin
+ */
